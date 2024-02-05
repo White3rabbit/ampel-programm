7924e88290930aef86765a3a602405bfbbc97789
White3rabbit <knechtli.kevin@hotmail.com>
2024-02-05 13:25:31 +0100

    speed + dir

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function main() {
-    var input = document.getElementById("age");
-    var age = input.value;
+    var inputSpeed = document.getElementById("Speed");
+    var inputDir = document.getElementById("Dir");
 
-    // < 6 jahre oder > 60 jahre -> sonderpreis 4 franken
-if(age <0 || age > 110) //  || = oder operator
-   alert("Bitte trage eine gültiges Alter ein");
-else if(age <=6 ||age >= 60) {
-    alert("Sonderpreis 4 franken");
-} else{
-alert("normalpreis 6.50 franken");
-}
-}
- // für alle anderen Altersgruppen -> 6.50
+    var Speed = inputSpeed.value;
+    var isFacing = inputDir.checked;
+
+
+
+console.log(speed);
+
+console.log(isFacing);
+
+} 
