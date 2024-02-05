214011e2f9de2681868039ea965a16dd74d8291f
White3rabbit <knechtli.kevin@hotmail.com>
2024-02-05 12:51:08 +0100

    kinokasse

--- main.js
+++ main.js
@@ -1,40 +1,14 @@
-// IF Statement
-
-//var age = prompt("Trage dein Alter ein: ");
-
-//if (age >= 18)// wird als false angegeben
-//{
-//Wird ausgeführt wenn Bediengung true ist
-//  console.log("Eintritt gewährt")
-//} else {
-//Wird in jedem anderen Fall ausgeführt
-//  console.log("Eintritt verweigert")
-//}
-
 function main() {
-    var input = document.getElementById("inputColor");
-    
-    var color = input.value;
+    var input = document.getElementById("age");
+    var age = input.value;
 
-    var lowerColorName = color.toLowerCase();
-    
-
-    //Wenn grün -> fahren
-    if (lowerColorName == "grün") {
-        alert("Du darfst fahren");
-    } else if (lowerColorName == "orange") {
-        alert("Du darfst anfahren oder musst bremsen");
-    } else if (lowerColorName == "rot") {
-        alert("Du musst anhalten");
-    } else {
-        alert("Keine gültige Farbe eingetragen");
-    }
-
+    // < 6 jahre oder > 60 jahre -> sonderpreis 4 franken
+if(age <0 || age > 110) //  || = oder operator
+   alert("Bitte trage eine gültiges Alter ein");
+else if(age <=6 ||age >= 60) {
+    alert("Sonderpreis 4 franken");
+} else{
+alert("normalpreis 6.50 franken");
 }
-
-
-//Wenn gelb -> anfahren/bremsen
-
-//Wenn rot -> halten
-
-//Fehlermeldung zurückgeben bei falscher Farbe
+}
+ // für alle anderen Altersgruppen -> 6.50
