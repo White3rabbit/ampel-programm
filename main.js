0f2e2bb91b7df501785fdba0040a30ff06f6aa86
White3rabbit <knechtli.kevin@hotmail.com>
2024-02-12 12:57:26 +0100

    new

--- main.js
+++ main.js
@@ -1,66 +1,25 @@
-
-// Notendurschnitt berechnen mit einem Array
-
-
-//Array
-var students = [
-    {
-        "Name": "Max",
-        "Matrikelnummer": 11,
+//Klassen
+function main(){
+ 
+    var student = {
+        "Name": "Homer",
+        "Matrikelnummer": 1,
         "NC": 4
-    },
-    {
-        "Name": "Peter",
-        "Matrikelnummer": 22,
-        "NC": 5
-    },
-    {
-        "Name": "Stefan",
-        "Matrikelnummer": 33,
-        "NC": 5.5
-    },
-    {
-        "Name": "Thomas",
-        "Matrikelnummer": 44,
-        "NC": 4.5
-    },
-    {
-        "Name": "Lukas",
-        "Matrikelnummer": 55,
-        "NC": 6
-    },
-]
+    };
 
-//Funktion Main
-function main() {
-    calcAverageNC();
+  
 }
 
-//Funktion Kalkulieren
-function calcAverageNC() {
+class Student {
 
-
-    //Variable innerhalb der Funktion
-    var totalNC = 0;
-
-    for (let i = 0; i < students.length; i++) {
-
-        //variable innerhalb der Schleife
-        var student = students[i];
-
-        //Ausgabe
-        console.log(student["Name"] + " hat einen NC von: " + student["NC"] + " und seine Martrikelnummer ist: " + student["Matrikelnummer"]);
-
-        //zusammenrechnen der Note
-        totalNC += student.NC
+    constructor(name, id, nc){
+        this.name = name;
+        this.id = id;
+        this.nc = nc;
     }
 
-    //durchschnittliche Note berechnen
-    var averageNC = totalNC / students.length;
-
-    //Ausgabe
-    console.log("Die durchschnittliche Note ist: " + averageNC);
 }
 
-
-
+var student = new Student("Homer", 1, 2);
+var student2 = new Student("Lisa", 2, 6);
+var student3 = new Student("Bart", 1, 3);
